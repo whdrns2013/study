4432d7e04d70eb0450e973c2089dae7b51ddcc03
whdrns2013@naver.com <62002568+whdrns2013@users.noreply.github.com>
2023-04-23 00:06:29 +0900

    commit

--- c_practice.c
+++ c_practice.c
@@ -4,8 +4,7 @@ void main() {
     char *p = "KOREA";
     printf("%s\n", p);
     printf("%s\n", p+3);
-    printf("%c\n", p);
     printf("%c\n", *p);
-    printf("%c\n", p+3);
     printf("%c\n", *(p+3));
+    printf("%c\n", *p + 2);
 }
