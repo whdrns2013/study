7964ddabd94fedff16d7387bb28756689bd026ca
whdrns2013@naver.com <62002568+whdrns2013@users.noreply.github.com>
2023-04-23 00:14:34 +0900

    Create q4_9.java

---
+++ q4_9.java
@@ -0,0 +1,12 @@
+public class q4_9 {
+    public static void main (String[] args) {
+        int[][] arry = new int [4][6];
+        for (int i = 0; i < 3; i ++){
+            for (int j = 0; j < 5; j++) {
+                arry[i][j] = j * 3 + ( i + 1 );
+                System.out.print(arry[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+}
