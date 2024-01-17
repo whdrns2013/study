82b1e324968ae71c6ce7c9e7701111f4b8495225
Jonghyuk Park <whdrns2013@naver.com>
2024-01-18 08:48:08 +0900

    Update 08_01_func_defining.dart

--- 02_Dart/08_function/08_01_func_defining.dart
+++ 02_Dart/08_function/08_01_func_defining.dart
@@ -2,5 +2,5 @@ int func01(int firstNum, int secondNum) => firstNum + secondNum;
 int func02({int firstNum=0, int secondNum=0}) => firstNum + secondNum;
 
 void main(){
-  print(func01(int firstNum:10, int secondNum:20));
+  print(func01(firstNum:10, secondNum:20));
 }
