07cfd157a3c2b9c0b932068d43436332677bf99c
whdrns2013@naver.com <62002568+whdrns2013@users.noreply.github.com>
2024-01-27 22:15:57 +0900

    study flutter

--- 02_Flutter/05_UI/ui_header/lib/main.dart
+++ 02_Flutter/05_UI/ui_header/lib/main.dart
@@ -26,11 +26,12 @@ class App extends StatelessWidget {
                         fontSize: 28,
                         fontWeight: FontWeight.w800,
                       ),
-                    ),
+                   ),
                     Text(
                       "Welcome Back",
                       style: TextStyle(
-                        color: Colors.white.withOpacity(0.8),
+                        // color: Colors.white.withOpacity(0.8),
+                        color: Color.fromRGBO(0, 0, 0, 0.8),
                         fontSize: 16,
                       ),
                     ),
