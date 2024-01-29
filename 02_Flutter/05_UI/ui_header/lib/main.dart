880d2762a214291414a4a658711e44382fe51656
whdrns2013@naver.com <62002568+whdrns2013@users.noreply.github.com>
2024-01-29 23:41:40 +0900

    study flutter

--- 02_Flutter/05_UI/ui_header/lib/main.dart
+++ 02_Flutter/05_UI/ui_header/lib/main.dart
@@ -9,37 +9,106 @@ class App extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-          backgroundColor: Color(0xFF181818),
-          body: Row(
-            mainAxisAlignment: MainAxisAlignment.end,
+        backgroundColor: Color(0xFF181818),
+        body: Padding(
+          padding: EdgeInsets.symmetric(horizontal: 30),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
             children: [
-              Padding(
-                padding: EdgeInsets.symmetric(horizontal: 30),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.end,
-                  children: [
-                    SizedBox(height: 55),
-                    Text(
-                      "Hi, Selena",
-                      style: TextStyle(
-                        color: Colors.white,
-                        fontSize: 28,
-                        fontWeight: FontWeight.w800,
+              SizedBox(
+                height: 55,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.end,
+                children: [
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.end,
+                    children: [
+                      Text(
+                        "Hi, Selena",
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 28,
+                          fontWeight: FontWeight.w800,
+                        ),
                       ),
-                   ),
-                    Text(
-                      "Welcome Back",
-                      style: TextStyle(
-                        // color: Colors.white.withOpacity(0.8),
-                        color: Color.fromRGBO(0, 0, 0, 0.8),
-                        fontSize: 16,
+                      Text(
+                        "Welcome Back",
+                        style: TextStyle(
+                          // color: Colors.white.withOpacity(0.8),
+                          color: Color.fromRGBO(255, 255, 255, 0.8),
+                          fontSize: 16,
+                        ),
                       ),
-                    ),
-                  ],
+                    ],
+                  ),
+                ],
+              ),
+              SizedBox(
+                height: 80,
+              ),
+              Text(
+                "Total Balance",
+                style: TextStyle(
+                  color: Color.fromRGBO(255, 255, 255, 0.85),
+                  fontSize: 22,
                 ),
               ),
+              SizedBox(
+                height: 10,
+              ),
+              Text(
+                "\$7 123 456",
+                style: TextStyle(
+                  color: Color.fromRGBO(255, 255, 255, 1),
+                  fontSize: 48,
+                  fontWeight: FontWeight.w700,
+                ),
+              ),
+              SizedBox(
+                height: 20,
+              ),
+              Row(
+                children: [
+                  Container(
+                    decoration: BoxDecoration(
+                      color: Colors.amber,
+                      borderRadius: BorderRadius.circular(45),
+                    ),
+                    child: Padding(
+                      padding: EdgeInsets.symmetric(
+                        vertical: 15,
+                        horizontal: 45,
+                      ),
+                      child: Text(
+                        "Transfer",
+                        style: TextStyle(fontSize: 22),
+                      ),
+                    ),
+                  ),
+                  SizedBox(width: 15,),
+                  Container(
+                    decoration: BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.circular(45),
+                    ),
+                    child: Padding(
+                      padding: EdgeInsets.symmetric(
+                        vertical: 15,
+                        horizontal: 45,
+                      ),
+                      child: Text(
+                        "Request",
+                        style: TextStyle(fontSize: 22),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
             ],
-          )),
+          ),
+        ),
+      ),
     );
   }
 }
