438343e6bd4bd90e12988884e76d5f4bcd107a9a
jonghyuk.park <whdrns2013@naver.com>
2025-06-17 13:33:17 +0900

    Update test_ssh2.rs

--- 06_Rust/99_ssh/ssh/src/test_ssh2.rs
+++ 06_Rust/99_ssh/ssh/src/test_ssh2.rs
@@ -2,12 +2,11 @@ use ssh2::Session;
 use std::net::TcpStream;
 use std::io::Read;
 
-
-pub fn do_ssh(host: &str,
-              port: &str,
-              user: &str,
-              password: &str,
-              command: &str) -> Result<String, String>{
+// SSH 세션 생성
+pub fn create_session(host: &str,
+                      port: &str,
+                      user: &str,
+                      password: &str) -> Result<Session, String>{
     // TCP Connection
     let tcp = TcpStream::connect(format!("{host}:{port}"))
         .map_err(|e| format!("Failed to connect{}", e))?;
@@ -24,7 +23,12 @@ pub fn do_ssh(host: &str,
     session.userauth_password(user, password)
         .map_err(|e| format!("Authentication failed: {}", e))?;
     println!("success to Authentication");
+    
+    Ok(session)
+}
 
+// SSH 명령어 수행
+pub fn run_command(session: &Session, command: &str) -> Result<String, String>{
     // run command
     let mut channel = session.channel_session().map_err(|e| format!("Channel failed: {}", e))?;
     channel.exec(command).map_err(|e| format!("Command failed: {}", e))?;
@@ -35,5 +39,12 @@ pub fn do_ssh(host: &str,
     channel.read_to_string(&mut output).map_err(|e| format!("Read failed: {}", e))?;
 
     println!("{}", output);
+    Ok(output)
+}
+
+// SSH 세션 종료
+pub fn close_session(session: Session) -> Result<String, String>{
+    drop(session);
+    let mut output = "closing session success".to_string();
     Ok(output)
 }
