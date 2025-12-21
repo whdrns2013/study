2aadfad7090c5557bc9b3fb61da2764af1278100
jonghyuk.park <whdrns2013@naver.com>
2025-12-22 08:55:39 +0900

    Update evaluator.py

--- 09_recommendation/10_metric/metric_src/evaluator.py
+++ 09_recommendation/10_metric/metric_src/evaluator.py
@@ -3,90 +3,87 @@ from utils.logger.default_logger import logger
 from schemas.dto import TestCase
 from core.enums import MetricMethod
 import math
+from abc import ABC, abstractmethod
 
-# TODO: 예외처리, 로그 (코드 전반)
-# TODO: DB에 저장 미구현
-
-class Evaluator:
+class Evaluator(ABC):
+    @abstractmethod
     def compute(self, case:TestCase, k:int|None=None) -> float:
-        raise NotImplementedError
+        pass
 
 class PrecisionEvaluator(Evaluator):
     def compute(self, case:TestCase, k:int|None=None) -> float:
         predict_items = case.predict_items
         truth_items = {key for key, relevance in case.ground_truth.items() if relevance > 0}
-        if k is None:
+        if (k is None) or (k > len(predict_items)):
             k = len(predict_items)
         predict_items_k = predict_items[:k]
         precision = self.calc_precision(predict_items_k, truth_items)
         return precision
-    
-    def calc_precision(self, predict_items, truth_items) -> float:
-        if (len(truth_items) == 0) or (len(predict_items) == 0):
+    def calc_precision(self, predict_items:list[str|int],
+                       truth_items:set[str|int]) -> float:
+        if len(predict_items) == 0:
             return 0.0
-        precision = len(set(predict_items) & set(truth_items)) / len(predict_items)
-        # 추천 리스트에는 중복이 없다고 가정한다.
+        precision = len(set(predict_items) & truth_items) / len(predict_items)
         return precision
 
 class RecallEvaluator(Evaluator):
     def compute(self, case:TestCase, k:int|None=None) -> float:
         predict_items = case.predict_items
         truth_items = {key for key, relevance in case.ground_truth.items() if relevance > 0}
-        if k is None:
+        if (k is None) or (k > len(predict_items)):
             k = len(predict_items)
         predict_items_k = predict_items[:k]
         recall = self.calc_recall(predict_items_k, truth_items)
         return recall
-    
-    def calc_recall(self, predict_items, truth_items) -> float:
-        if (len(truth_items) == 0) or (len(predict_items) == 0):
+    def calc_recall(self, predict_items:list[str|int],
+                    truth_items:set[str|int]) -> float:
+        if len(truth_items) == 0:
             return 0.0
-        recall = len(set(predict_items) & set(truth_items)) / len(set(truth_items))
+        recall = len(set(predict_items) & truth_items) / len(truth_items)
         return recall
-
+    
 class RREvaluator(Evaluator):
-    def compute(self, case:TestCase, k:int|None=None):
+    def compute(self, case:TestCase, k:int|None=None) -> float:
         predict_items = case.predict_items
         truth_items = {key for key, relevance in case.ground_truth.items() if relevance > 0}
-        if k is None:
+        if (k is None) or (k > len(predict_items)):
             k = len(predict_items)
         predict_items_k = predict_items[:k]
         rr = self.calc_rr(predict_items_k, truth_items)
         return rr
-    
-    def calc_rr(self, predict_items, truth_items) -> float:
-        # RR은 **선호도에 상관 없이**, 가장 빨리 출현한 선호 아이템을 기준으로 계산한다.
-        if (len(truth_items) == 0) or (len(predict_items) == 0):
+    def calc_rr(self, predict_items:list[str|int],
+                truth_items:set[str|int]) -> float:
+        if len(predict_items) == 0:
             return 0.0
         for i, item in enumerate(predict_items):
             if item in truth_items:
-                return 1 / (i + 1)
+                return 1/(i+1)
         return 0.0
 
 class APEvaluator(Evaluator):
-    def compute(self, case:TestCase, k:int|None=None):
+    def compute(self, case:TestCase, k:int|None=None) -> float:
         predict_items = case.predict_items
         truth_items = {key for key, relevance in case.ground_truth.items() if relevance > 0}
-        if k is None:
+        if (k is None) or (k > len(predict_items)):
             k = len(predict_items)
         predict_items_k = predict_items[:k]
         ap = self.calc_ap(predict_items_k, truth_items)
         return ap
-    
-    def calc_ap(self, predict_items, truth_items) -> float:
-        if (len(truth_items) == 0) or (len(predict_items) == 0):
+    # 본 AP는 실무적으로 커스터마이징한 지표임
+    def calc_ap(self, predict_items:list[str|int],
+                truth_items:set[str|int]) -> float:
+        if len(truth_items) == 0:
             return 0.0
-        truth_items = set(truth_items)
         sum_precision = 0
         hit = 0
-        for i, item in enumerate(predict_items):
-            if item in truth_items:
+        for i, item_yn in enumerate(predict_items):
+            if item_yn in truth_items:
                 hit += 1
-                sum_precision += hit/(i+1)
-        return sum_precision/len(truth_items)
+                sum_precision += hit/(i + 1)
+        return sum_precision/min(len(truth_items), len(predict_items))
             
 class NDCGEvaluator(Evaluator):
-    def compute(self, case:TestCase, k:int|None=None):
+    def compute(self, case:TestCase, k:int|None=None) -> float:
         predict_items = case.predict_items
         ground_truth = case.ground_truth
         if (k is None) or (k > len(predict_items)):
@@ -95,21 +92,22 @@ class NDCGEvaluator(Evaluator):
         ndcg = self.calc_ndcg(predict_items_k, ground_truth, k)
         return ndcg
     
-    def calc_dcg(self, scores) -> float:
-        dcg = sum((score / math.log2(i+1+1)) for i, score in enumerate(scores))
+    def calc_dcg(self, relevance_scores) -> float:
+        dcg = sum(score / (math.log2(i+1+1)) for i, score in enumerate(relevance_scores))
         return dcg
     
-    def calc_ndcg(self, predict_items, ground_truth, k) -> float:
+    def calc_ndcg(self, predict_items:list[str|int],
+                  ground_truth:dict[str|int, float],
+                  k:int=None) -> float:
         if len(ground_truth) == 0:
             return 0.0
-        if (k is None) or (k > len(predict_items)):
-            k = len(predict_items)
         ideal_relevance_scores = sorted(ground_truth.values(), reverse=True)[:k]
         idcg = self.calc_dcg(ideal_relevance_scores)
         if idcg == 0.0:
             return 0.0
-        dcg = self.calc_dcg([ground_truth.get(item, 0.0) for item in predict_items][:k])
+        dcg = self.calc_dcg([ground_truth.get(item, 0.0) for item in predict_items])
         return dcg/idcg
+    
 
 class EvaluatorFactory:
     _map = {
@@ -123,7 +121,9 @@ class EvaluatorFactory:
     def create(cls, metric: MetricMethod) -> Evaluator:
         try:
             evaluator = cls._map[metric]
-        except:
+        except ValueError:
             raise ValueError(f"There is no metric {metric}")
+        except KeyError:
+            raise KeyError(f"There is no metric {metric}")
         return evaluator()
     
