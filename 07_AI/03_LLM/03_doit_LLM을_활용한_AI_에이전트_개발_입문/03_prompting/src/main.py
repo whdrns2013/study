02e6ec1d813c96d92199df96af5bdbe9c8c4f49d
Jonghyuk Park <whdrns2013@naver.com>
2026-01-20 07:41:59 +0900

    study prompting

---
+++ 07_AI/03_LLM/03_doit_LLM을_활용한_AI_에이전트_개발_입문/03_prompting/src/main.py
@@ -0,0 +1,17 @@
+from core.config import config
+from feature.openai_api import chat_gpt
+
+def main():
+    
+    # 1. 
+    
+    # 2. 해리포터의 볼트모트
+    
+    user_message = "안녕하세요. 반갑습니다."
+    response = chat_gpt(user_message)
+    print(response)
+    print(response.choices[0].message.content)
+
+
+if __name__ == "__main__":
+    main()
